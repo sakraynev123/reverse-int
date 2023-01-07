88958133b8c1db50ef8e044bd07d5bde0bc4be16
Alexander <klivets.alex@gmail.com>
2023-01-07 21:58:09 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n.toString()[0] === '-') {
+        return n.toString().split('').splice(1).reverse().join('')
+      }
+      else if (n.toString()[n.toString().length - 1] === '0') {
+        return n.toString().split('').reverse().splice(1).join('')
+      }
+      else {
+        return n.toString().split('').reverse().join('')
+      }
 }
